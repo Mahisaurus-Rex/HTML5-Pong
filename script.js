15b7d4d4154c353f87835ac6c9871077d8db3883
Mahisaurus-Rex <mahipasarkar@gmail.com>
2019-06-21 22:58:38 -0400

    changes, still can't get controls

--- script.js
+++ script.js
@@ -9,23 +9,17 @@ canvas.height=window.innerHeight;
 //context variable
 var c = canvas.getContext('2d');
 
-//get controls
-canvas.addEventListener('keydown',this.check,false);
-updateKeys = function(e){
-  //set current key
-  currentKey = e.keyCode;
-  console.log("current key: " + currentKey);
- }
+// Handle keyboard controls
+var keysDown = {};
 
-//Keyboard constants
- K_A = 65; K_B = 66; K_C = 67; K_D = 68; K_E = 69; K_F = 70; K_G = 71;
- K_H = 72; K_I = 73; K_J = 74; K_K = 75; K_L = 76; K_M = 77; K_N = 78;
- K_O = 79; K_P = 80; K_Q = 81; K_R = 82; K_S = 83; K_T = 84; K_U = 85;
- K_V = 86; K_W = 87; K_X = 88; K_Y = 89; K_Z = 90;
- K_LEFT = 37; K_RIGHT = 39; K_UP = 38;K_DOWN = 40; K_SPACE = 32;
- K_ESC = 27; K_PGUP = 33; K_PGDOWN = 34; K_HOME = 36; K_END = 35;
- K_0 = 48; K_1 = 49; K_2 = 50; K_3 = 51; K_4 = 52; K_5 = 53;
- K_6 = 54; K_7 = 55; K_8 = 56; K_9 = 57;
+// Check for keys pressed where key represents the keycode captured
+addEventListener("keydown", function (key) {
+  keysDown[key.keyCode] = true;
+}, false);
+addEventListener("keyup", function (key) {
+  delete keysDown[key.keyCode];
+}, false);
+
 
 //initial variables
 var p1_x=30;
@@ -70,16 +64,22 @@ var Player2= new Paddle(p2_x,p2_y);
 function animate(){
 	//clear screen
 	c.clearRect(0,0,innerWidth,innerHeight);
-	//get control
-	document.onkeydown = updateKeys;
+
+	// Check for keys pressed where key represents the keycode captured
+	addEventListener("keydown", function (key) {
+	  keysDown[key.keyCode] = true;
+	}, false);
+	addEventListener("keyup", function (key) {
+	  delete keysDown[key.keyCode];
+	}, false);
+
 	//controls
-	/*
-	if(check()=="down"){
-		Player2.moveDown();
+	if (87 in keysDown) { // Player is holding W key
+	  Player2.moveUp();
 	}
-	*/
-
-
+	if (83 in keysDown) { // Player is holding down key
+	  Player2.moveDown();
+	}
 
 	//draw and update
 	Player1.draw();
